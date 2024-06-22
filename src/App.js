ec6081467e9f44ef14d57e1a4bd324e2a4734fae
Shubham Bisht <shubhambisht841@gmail.com>
2024-06-22 19:08:04 +0530

    commit to master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './Components/Layout'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+        </Routes>
+        <Routes>
+          <Route index element={<Home/>}/>
+        </Routes>
+        <Routes>
+          <Route path="/about" index element={<About/>}/>
+        </Routes>
+        <Routes>
+          <Route path="/contact" index element={<Contact/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Contact/index.js
@@ -0,0 +1,105 @@
+import './index.scss'
+import Loader from 'react-loaders'
+import Sidebar from '../Sidebar'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <Sidebar />
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially on ambitious
+            or large projects. However, if you have any other requests or
+            questions, don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="name"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="info-map">
+        Shubham Bisht,
+        <br />
+        India
+        <br />
+        Kamal Vihar Kamal Pur <br />
+        Burari <br />
+        <br />
+        <span>shubhambisht@gmail.com</span>
+      </div>
+
+      <div className="map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+export default Contact
